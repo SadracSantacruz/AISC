78b5a9016a6dfc4ecd9bf868ae2712c0e456ce75
Sadrac Santacruz <70788009+SadracSantacruz@users.noreply.github.com>
2025-04-06 13:40:28 -0700

    added animated image

--- aisc_website/components/Navbar/Navbar.tsx
+++ aisc_website/components/Navbar/Navbar.tsx
@@ -8,8 +8,21 @@ const Navbar: React.FC = () => {
       <div className="max-w-7xl mx-auto flex items-center justify-between">
         {/* Logo */}
         <Link href="/">
-          <div className="flex items-center space-x-2">
-            <Image src="/logo.png" alt="AISC Logo" width={40} height={40} />
+          <div className="relative w-10 h-10 group">
+            <Image
+              src="/logo.png"
+              alt="AISC Logo"
+              width={40}
+              height={40}
+              className="absolute transition-opacity duration-300 group-hover:opacity-0"
+            />
+            <Image
+              src="/logo-hover.png"
+              alt="AISC Logo Lit"
+              width={40}
+              height={40}
+              className="absolute transition-opacity duration-300 opacity-0 group-hover:opacity-100"
+            />
           </div>
         </Link>
 
